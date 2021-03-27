d250dd92fed336ec95c55a19818440c6f498a903
Anuj Mathur <anuj.mathur@rewardle.com>
2021-03-27 11:47:11 +0000

    Handled the error for unsupported routes

--- app.js
+++ app.js
@@ -2,11 +2,19 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const placesRouter = require('./routes/places.route')
 const usersRouter = require('./routes/users.routes')
+const HttpError = require('./models/http-error')
 const app = express()
 
 app.use(bodyParser.json())
 
 app.use('/api/places',placesRouter)
+
+app.use((req,res,next)=>{
+  const error = new HttpError("could not find the this routes",404)
+  throw error
+
+})
+
 app.use((error,req,res,next)=>{
     if(res.headerSent){
         return next(error)
