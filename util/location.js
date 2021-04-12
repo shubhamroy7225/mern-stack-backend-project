07a68336712ab2d42ce91a8b75a18e9dd0afe6ee
Anuj Mathur <anuj.mathur@rewardle.com>
2021-04-12 11:33:57 +0000

    Added google map feature

--- util/location.js
+++ util/location.js
@@ -1,31 +1,25 @@
-const API_KEY = process.env.GOOGLE_API_KEY
-const axios = require('axios');
-const HttpError = require('../models/http-error');
+const API_KEY = process.env.GOOGLE_API_KEY;
+const axios = require("axios");
+const HttpError = require("../models/http-error");
 async function getCoordsForAddress(address) {
-    
-    // return {
-    //   lat: 40.7484474,
-    //   lng: -73.9871516
-    // };
-    const response = await axios.get(
-      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
-        address
-      )}&key=${API_KEY}`
+  const response = await axios.get(
+    `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${API_KEY}`
+  );
+  const data = response.data;
+  console.log(data);
+  if (!data || data.status === "ZERO_RESULTS") {
+    const error = new HttpError(
+      "Could not find location for the specified address.",
+      422
     );
-  
-    const data = response.data;
-    console.log(data);
-    if (!data || data.status === 'ZERO_RESULTS') {
-      const error = new HttpError(
-        'Could not find location for the specified address.',
-        422
-      );
-      throw error;
-    }
-  
-    const coordinates = data.results[0].geometry.location;
-  
-    return coordinates;
+    throw error;
   }
-  
-  module.exports = getCoordsForAddress;
+  const coordinates = data.results[0].geometry.location;
+  // const coordinates = {
+  //   lat:22.3177406,
+  //   lng:73.1240113,
+  // }
+  return coordinates;
+}
+
+module.exports = getCoordsForAddress;
